a1c9609f7c1139c97ca9085878738585e7a79396
avi5kdon <avi5kdon@gmail.com>
2019-03-19 11:29:46 +0530

    gery5ty5y5

--- src/main/java/com/redhat/fuse/boosters/rest/http/Application.java
+++ src/main/java/com/redhat/fuse/boosters/rest/http/Application.java
@@ -8,8 +8,9 @@ public class Application {
 
     /**
      * Main method to start the application.
+     * 
      */
-    public static void main(String[] args) { 
+    public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
