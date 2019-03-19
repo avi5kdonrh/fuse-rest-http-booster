0f788e5f257e18c23cf9a73bb3d6e0394484016b
avi5kdon <avi5kdon@gmail.com>
2019-03-19 07:27:17 +0530

    Mess

--- src/main/java/com/redhat/fuse/boosters/rest/http/Application.java
+++ src/main/java/com/redhat/fuse/boosters/rest/http/Application.java
@@ -9,7 +9,7 @@ public class Application {
     /**
      * Main method to start the application.
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) { 
         SpringApplication.run(Application.class, args);
     }
 
